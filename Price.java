007a8d073f916eaa28c34dba7d80df9df6a14e3a
jehunyoo <dbwpgns0525@gmail.com>
2020-05-29 11:04:13 +0900

    Initial sharing of project

---
+++ Price.java
@@ -0,0 +1,17 @@
+
+/**
+ * 여기에 ABC 클래스 설명을 작성하십시오.
+ * 
+ * @author (2018315005 유제훈) 
+ * @version (2020.05.29)
+ */
+public class Price
+{
+    int age; // 변수정의
+    public static int ticket(int age){
+        if(age >= 16){
+        }
+        else if(age
+    }
+    
+}
